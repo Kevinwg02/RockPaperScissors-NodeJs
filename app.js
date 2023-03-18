5f124e26052e1f6af14c0fec2820d2d5662d8619
Kevinwg02 <71637950+Kevinwg02@users.noreply.github.com>
2023-03-18 18:14:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 console.log("WELCOME TO A GAME OF ROCK PAPER SCISSORS");
 var player1;
 var player2;
+
 function gameRules(player1, player2) {
     if (player1 === "rock" && player2 === "scissors" || player1 === "paper" && player2 === "rock" || player1 === "scissors" && player2 === "paper") {
         console.log("player 1 wins");
     } else if (player1 === "rock" && player2 === "rock" || player1 === "paper" && player2 === "paper" || player1 === "scissors" && player2 === "paper") {
         console.log("tied");
-    }  else if (player2 === "rock" && player1 === "scissors" || player2 === "paper" && player1 === "rock" || player2 === "scissors" && player1 === "paper") {
+    } else if (player2 === "rock" && player1 === "scissors" || player2 === "paper" && player1 === "rock" || player2 === "scissors" && player1 === "paper") {
         console.log("player 2 wins");
-    }else{
+    } else {
         console.log("wrong input check your spelling")
     }
 }
-// lunching game by reading the input of player1
-const readline = require("readline")
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-rl.question("player 1, you're move ? ", function saveInput(player1) {
-// reading the input of player2
-    rl.question("player 2, you're move ? ", function saveInput(player2) {
-        // closing the intake
-        rl.close();
-        // lunching all the condition of comparaison with both valued filled
-        gameRules(player1, player2)
+
+function lunchingGame() {
+    const readline = require("readline")
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
     });
-});
+
+    rl.question("player 1, you're move ? ", function saveInput(player1) {
+        // reading the input of player2
+        rl.question("player 2, you're move ? ", function saveInput(player2) {
+            // closing the intake
+            rl.close();
+            // lunching all the condition of comparaison with both valued filled
+            gameRules(player1, player2)
+        });
+    });
+
+}
+
+lunchingGame();
+
 
