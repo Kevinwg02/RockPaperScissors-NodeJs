4f185d5680b61514ae9f77b3bd36ba5515d01321
Kevinwg02 <kevin.waag@outlook.com>
2023-06-30 15:36:07 +0200

    bored

--- NOToptimized-version.js
+++ NOToptimized-version.js
@@ -1,131 +1,84 @@
-const prompt = require("password-prompt");
-const readline = require("readline");
+namep1 = "john";
+namep2 = "mia";
+let scorep1;
+let scorep2;
 
-const moves = {
-  1: "feuille",
-  2: "pierre",
-  3: "ciseaux",
+
+function playAgain() {
+  const readline = require("readline")
+  const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+  });
+
+  rl.question("On joue encore une fois ? o - n ", function saveInput(staygo) {
+      rl.close();
+      if (staygo.toLowerCase() === "o") {
+          Game();
+      } else if (staygo.toLowerCase() === "n") {
+          scoreBoard();
+          exitForReal();  
+      }       
+  });
+}
+function exitForReal() {
+  const readline = require("readline")
+  const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+  });
+  rl.question("Appuis sur ENTRER", function saveInput(staygo) {
+      rl.close();
+  });
+}
+function scoreBoard() {
+  console.log("Scores Final:");
+  console.log(namep1 + ": " + scorep1);
+  console.log(namep2 + ": " + scorep2);
+}
+
+const rules = {
+  feuille: { ciseaux: false, pierre: true },
+  pierre: { ciseaux: true, feuille: false },
+  ciseaux: { feuille: true, pierre: false },
 };
 
-let player1;
-let player2;
-let namep1;
-let namep2 = "bot";
-let scorep1 = 0;
-let scorep2 = 0;
-
-function uselessStars() {
-  console.log("********************************************************************************");
-}
-
-function welcomeMSG() {
-  uselessStars();
-  console.log("                  BIENVENUE AU PIERRE, FEUILLE, CISEAUX                    ");
-  uselessStars();
-}
-
-function gameRules(player1, player2) {
-  if (
-    (player1 === "pierre" && player2 === "ciseaux") ||
-    (player1 === "feuille" && player2 === "pierre") ||
-    (player1 === "ciseaux" && player2 === "feuille")
-  ) {
-    console.log(`Felicitation ${namep1}! Tu as gagné`);
-    scorep1 += 1;
-    playAgain();
-  } else if (
-    (player2 === "pierre" && player1 === "ciseaux") ||
-    (player2 === "feuille" && player1 === "pierre") ||
-    (player2 === "ciseaux" && player1 === "feuille")
-  ) {
-    console.log(`Felicitation ${namep2}! Tu as gagné`);
-    scorep2 += 1;
-    playAgain();
-  } else if (
-    (player1 === "pierre" && player2 === "pierre") ||
-    (player1 === "feuille" && player2 === "feuille") ||
-    (player1 === "ciseaux" && player2 === "ciseaux")
-  ) {
-    console.log("Egalité");
-    playAgain();
+function determineWinner(player1, player2) {
+  if (rules[player1][player2]) {
+    return "player1";
+  } else if (rules[player2][player1]) {
+    return "player2";
+  } else {
+    return "tie";
   }
 }
 
-function randomBotMove() {
-  const random = Math.floor(Math.random() * 3) + 1;
-  return moves[random];
+function playGame(player1, player2) {
+  const winner = determineWinner(player1, player2);
+
+  if (winner === "player1") {
+    console.log(`Félicitations ${namep1}! Tu as gagné.`);
+    scorep1 += 1;
+  } else if (winner === "player2") {
+    console.log(`Félicitations ${namep2}! Tu as gagné.`);
+    scorep2 += 1;
+  } else {
+    console.log("Égalité.");
+  }
+
+  playAgain();
 }
 
-async function game() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+const prompt = require('password-prompt');
+async function Game() {
+    numberp1 = await prompt(namep1 + ': Appuis 1 pour feuille, 2 pour pierres, 3 pour ciseaux: ', {
+        method: 'hide'
+    });
+    numberp2 = await prompt(' et maintenant pour ' + namep2 + ': ', {
+        method: 'hide'
+    });
 
-  const numberp1 = await prompt(`${namep1}: Appuie 1 pour feuille, 2 pour pierre, 3 pour ciseaux: `, {
-    method: "hide",
-  });
-
-  player1 = moves[parseInt(numberp1)];
-  player2 = randomBotMove();
-
-  console.log(`${player1} vs ${player2}`);
-  gameRules(player1, player2);
-
-  rl.close();
 }
 
-function exitForReal() {
-  readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  }).question("Appuie sur ENTRER", () => {
-    process.exit();
-  });
-}
 
-function lunchGameNames() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  rl.question("Joueur 1, écris ton nom: ", (name1) => {
-    namep1 = name1;
-
-    uselessStars();
-    console.log("Bonne chance");
-    uselessStars();
-
-    game();
-
-    rl.close();
-  });
-}
-
-function playAgain() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  rl.question("On joue encore une fois ? (o/n): ", (staygo) => {
-    if (staygo.toLowerCase() === "o") {
-      game();
-    } else if (staygo.toLowerCase() === "n") {
-      scoreBoard();
-      exitForReal();
-    }
-
-    rl.close();
-  });
-}
-
-function scoreBoard() {
-  console.log("Scores finaux:");
-  console.log(`${namep1}: ${scorep1}`);
-  console.log(`${namep2}: ${scorep2}`);
-}
-
-welcomeMSG();
-lunchGameNames();
+playGame("pierre", "feuille");
