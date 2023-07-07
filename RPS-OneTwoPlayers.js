78dc679bb81c6d178e068efef91a26e0da285031
Kevinwg02 <kevin.waag@outlook.com>
2023-07-07 09:06:46 +0200

    toujours pas

---
+++ RPS-OneTwoPlayers.js
@@ -0,0 +1,237 @@
+var player1;
+var player2;
+var numberp1;
+var numberp2;
+var namep1;
+var namep2 = "Jerry";
+var nbplayers;
+var scorep1 = 0;
+var scorep2 = 0;
+var random;
+
+//useless text for presentation
+function uselessStars() {
+  console.log(
+    "********************************************************************************"
+  );
+  console.log(
+    "********************************************************************************"
+  );
+  console.log(
+    "********************************************************************************"
+  );
+  console.log(
+    "********************************************************************************"
+  );
+  console.log(
+    "********************************************************************************"
+  );
+  console.log(
+    "********************************************************************************"
+  );
+}
+
+function welcomeMSG() {
+  uselessStars();
+  console.log(
+    "                  BIENVENUE AU PIERRE, FEUILLE, CISEAUX                    "
+  );
+  uselessStars();
+}
+
+function exitForReal() {
+  //dumy entry to force the terminal to stop and not close
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question("Appuis sur ENTRER", function saveInput() {
+    rl.close();
+  });
+}
+
+function gameRules(player1, player2) {
+  if (
+    (player1 === "pierre" && player2 === "ciseaux") ||
+    (player1 === "feuille" && player2 === "pierre") ||
+    (player1 === "ciseaux" && player2 === "feuille")
+  ) {
+    console.log(
+      "                          Felicitation " +
+        namep1 +
+        "! Tu as gagné                      "
+    );
+    //up the score by one for the scoreboard
+    scorep1 += 1;
+    playAgain(nbplayers);
+  } else if (
+    (player2 === "pierre" && player1 === "ciseaux") ||
+    (player2 === "feuille" && player1 === "pierre") ||
+    (player2 === "ciseaux" && player1 === "feuille")
+  ) {
+    console.log(
+      "                        Felicitation " +
+        namep2 +
+        "! Tu as gagné                        "
+    );
+    scorep2 += 1;
+    playAgain(nbplayers);
+  } else if (
+    player1 === player2
+  ) {
+    console.log("Egalité");
+    playAgain(nbplayers);
+  }
+}
+
+function associateMove(numberp1, numberp2) {
+  switch (numberp1) {
+    case 1:
+      player1 = "feuille";
+      break;
+    case 2:
+      player1 = "pierre";
+      break;
+    case 3:
+      player1 = "ciseaux";
+      break;
+    default:
+      console.log(player1 + " fautes de frappe");
+  }
+  switch (numberp2) {
+    case 1:
+      player2 = "feuille";
+      break;
+    case 2:
+      player2 = "pierre";
+      break;
+    case 3:
+      player2 = "ciseaux";
+      break;
+    default:
+      console.log(player2 + " fautes de frappe");
+  }
+  console.log(player1 + " vs " + player2);
+  gameRules(player1, player2);
+}
+
+function randomBotmove() {
+  const crypto = require("crypto");
+  const random = crypto.randomInt(1, 4); // Generate a random number between 1 and 3
+  return random;
+}
+
+const Gameprompt = require("password-prompt");
+async function Game() {
+  //using a password prompt to hide the input
+  numberp1 = await Gameprompt(
+    namep1 + ": Appuie 1 pour feuille, 2 pour pierre, 3 pour ciseaux: ",
+    {
+      method: "hide",
+    }
+  );
+  numberp2 = await Gameprompt("Et maintenant pour " + namep2 + ": ", {
+    method: "hide",
+  });
+
+  associateMove(parseInt(numberp1), parseInt(numberp2));
+}
+
+const Botgameprompt = require("password-prompt");
+async function GameVersusbot() {
+  numberp1 = await Botgameprompt(
+    namep1 + ": Appuie 1 pour feuille, 2 pour pierre, 3 pour ciseaux: ",
+    {
+      method: "hide",
+    }
+  );
+
+  associateMove(parseInt(numberp1), numberp2);
+}
+
+function launchGame(gameMode) {
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question("Joueur 1, écris ton nom ", function saveInput(name1) {
+    if (gameMode === "twoplayer") {
+      rl.question("Et maintenant joueur 2: ", function saveInput(name2) {
+        rl.close();
+        namep1 = name1;
+        namep2 = name2;
+
+        uselessStars();
+        console.log("                                 Bonne Chance                                  ");
+        uselessStars();
+        Game();
+      });
+    } else if (gameMode === "bot") {
+      rl.close();
+      namep1 = name1;
+      numberp2 = randomBotmove();
+      uselessStars();
+      console.log("                                 Bonne Chance                                  ");
+      uselessStars();
+      GameVersusbot();
+    }
+  });
+}
+
+function numberofGames() {
+  const readline = require("readline");
+  const rl0 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl0.question("Vous jouez seul ou à deux ? Appuie sur 1 ou 2 ", function saveInput(
+    nbplayers
+  ) {
+    nbplayers = parseInt(nbplayers);
+    rl0.close();
+
+    //decide if we play alone or against someone
+    if (nbplayers === 1) {
+      launchGame("bot");
+    } else if (nbplayers === 2) {
+      launchGame("twoplayer");
+    }
+  });
+}
+
+function playAgain(gameMode) {
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question("On joue encore une fois ? o - n ", function saveInput(staygo) {
+    rl.close();
+    if (staygo.toLowerCase() === "o") {
+      numberp1 = null;
+      numberp2 = null;
+      player1 = null;
+      player2 = null;
+      gameRules(null, null); // Reset the game state
+      launchGame(gameMode); // Restart the game with the previous game mode
+    } else if (staygo.toLowerCase() === "n") {
+      scoreBoard();
+      exitForReal();
+    }
+  });
+}
+
+
+
+function scoreBoard() {
+  console.log("SCORE FINAL:");
+  console.log(namep1 + ": " + scorep1);
+  console.log(namep2 + ": " + scorep2);
+}
+
+welcomeMSG();
+numberofGames();
