94878a0ad0ab6e69b763b5db906c9457e4fd5cdf
kevinwg02 <kevin.waag@outlook.com>
2023-07-16 22:31:55 +0200

    works perfectly now

--- RPS-OnePlayer.js
+++ RPS-OnePlayer.js
@@ -4,7 +4,7 @@ var numberp1;
 var numberp2;
 var namep1;
 var namep2 = "Jerry";
-var nbplayers;
+var staygo;
 var scorep1 = 0;
 var scorep2 = 0;
 var random;
@@ -36,18 +36,7 @@ function welcomeMSG() {
   );
   uselessStars();
 }
-function exitForReal() {
-  //dumy entry to force the terminal to stop and not close
-  const readline = require("readline");
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question("Appuis sur ENTRER", function saveInput() {
-    rl.close();
-  });
-}
-function gameRules(player1, player2, nbplayers) {
+function gameRules(player1, player2) {
   if (
     (player1 === "pierre" && player2 === "ciseaux") ||
     (player1 === "feuille" && player2 === "pierre") ||
@@ -55,13 +44,11 @@ function gameRules(player1, player2, nbplayers) {
   ) {
     console.log(
       "                          Felicitation " +
-        namep1 +
-        "! Tu as gagner                      "
+      namep1 +
+      "! Tu as gagner                      "
     );
-    //up the score by one for the scoreboard
     scorep1 += 1;
-    definePlayagain(nbplayers);
-    //playagain ici
+    playAgain();
   } else if (
     (player2 === "pierre" && player1 === "ciseaux") ||
     (player2 === "feuille" && player1 === "pierre") ||
@@ -69,12 +56,11 @@ function gameRules(player1, player2, nbplayers) {
   ) {
     console.log(
       "                        Felicitation " +
-        namep2 +
-        "! Tu as gagner                        "
+      namep2 +
+      "! Tu as gagner                        "
     );
     scorep2 += 1;
-    definePlayagain(nbplayers);
-    //fonction playagain ici
+    playAgain();
   } else if (
     (player1 === "pierre" && player2 === "pierre") ||
     (player1 === "feuille" && player2 === "feuille") ||
@@ -97,6 +83,7 @@ function associateMove(numberp1, numberp2) {
       break;
     default:
       console.log(player1 + " fautes de frappes");
+      return;
   }
   switch (numberp2) {
     case 1:
@@ -110,71 +97,45 @@ function associateMove(numberp1, numberp2) {
       break;
     default:
       console.log(player2 + " fautes de frappes");
+      return;
   }
   console.log(player1 + " vs " + player2);
   gameRules(player1, player2);
 }
+
 function randomBotmove() {
-  const crypto = require("crypto");
+  const crypto = require('crypto');
   const random = crypto.randomInt(1, 4); // Generate a random number between 0 and 2
   return random;
 }
-function scoreBoard() {
-  console.log("SCORE FINALE:");
-  console.log(namep1 + ": " + scorep1);
-  console.log(namep2 + ": " + scorep2);
-}
 
-const Gameprompt = require("password-prompt");
+const prompt = require("password-prompt");
 async function Game() {
-  //using a password prompt to hide the imput
-  numberp1 = await Gameprompt(
+  numberp1 = await prompt(
     namep1 + ": Appuis 1 pour feuille, 2 pour pierres, 3 pour ciseaux: ",
     {
       method: "hide",
     }
   );
-  numberp2 = await Gameprompt(" et maintenant pour " + namep2 + ": ", {
-    method: "hide",
-  });
-
-  associateMove(parseInt(numberp1), parseInt(numberp2));
-}
-const Botgameprompt = require("password-prompt");
-async function GameVersusbot() {
-  numberp1 = await Botgameprompt(
-    namep1 + ": Appuis 1 pour feuille, 2 pour pierres, 3 pour ciseaux: ",
-    {
-      method: "hide",
-    }
-  );
+  if (parseInt(numberp1) !== 1 && parseInt(numberp1) !== 2 && parseInt(numberp1) !== 3) {
+    console.log("Numéro invalide pour " + namep1);
+    Game(); // Restart the game if the input is invalid
+    return; // Add a return statement to prevent further execution
+  }
   associateMove(parseInt(numberp1), numberp2);
 }
 
-function lunchTwoplayergame() {
+function exitForReal() {
   const readline = require("readline");
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-
-  rl.question("Joueur 1, écris ton nom ", function saveInput(name1) {
-    rl.question("et maintenant joueur 2: ", function saveInput(name2) {
-      rl.close();
-      namep1 = name1;
-      namep2 = name2;
-
-      uselessStars();
-      console.log(
-        "                                 Bonne Chance                                  "
-      );
-      uselessStars();
-      Game(playAgain());
-    });
+  rl.question("Appuis sur ENTRER", function saveInput(staygo) {
+    rl.close();
   });
 }
-
-function lunchBotgame() {
+function lunchGameNames() {
   const readline = require("readline");
   const rl = readline.createInterface({
     input: process.stdin,
@@ -190,58 +151,9 @@ function lunchBotgame() {
       "                                 Bonne Chance                                  "
     );
     uselessStars();
-    GameVersusbot();
+    Game();
   });
 }
-function numberofPlayers() {
-  const readline = require("readline");
-  const rl0 = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl0.question(
-    "Vous jouez a seul ou a deux ?  Appuis sur 1 ou 2 ",
-    function saveInput(nbplayers) {
-      nbplayers = parseInt(nbplayers);
-      rl0.close();
-
-      //decide if we play alone or against a someone
-      if (nbplayers === 1) {
-        lunchBotgame();
-      } else if (nbplayers === 2) {
-        lunchTwoplayergame();
-      }
-      return nbplayers;
-    }
-  );
-}
-function definePlayagain(nbplayers) {
-  if (nbplayers === 2) {
-    playAgain();
-  } else {
-    playAgainwithBot();
-  }
-}
-
-function playAgainwithBot() {
-  const readline = require("readline");
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  rl.question("On joue encore une fois ? o - n : ", function saveInput(staygo) {
-    rl.close();
-    if (staygo.toLowerCase() === "o") {
-      numberp2 = randomBotmove();
-      GameVersusbot();
-    } else if (staygo.toLowerCase() === "n") {
-      scoreBoard();
-      exitForReal();
-    }
-  });
-}
-
 function playAgain() {
   const readline = require("readline");
   const rl = readline.createInterface({
@@ -249,9 +161,10 @@ function playAgain() {
     output: process.stdout,
   });
 
-  rl.question("On joue encore une fois ? o - n : ", function saveInput(staygo) {
+  rl.question("On joue encore une fois ? o - n ", function saveInput(staygo) {
     rl.close();
     if (staygo.toLowerCase() === "o") {
+      numberp2 = randomBotmove();
       Game();
     } else if (staygo.toLowerCase() === "n") {
       scoreBoard();
@@ -259,6 +172,10 @@ function playAgain() {
     }
   });
 }
-
+function scoreBoard() {
+  console.log("SCORE FINALE:");
+  console.log(namep1 + ": " + scorep1);
+  console.log(namep2 + ": " + scorep2);
+}
 welcomeMSG();
-numberofPlayers();
+lunchGameNames();
